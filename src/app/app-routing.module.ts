c3b6ffaabf46e29cf9d9e0d9f5492475f95e9f6c
Philippe Martin <contact@elol.fr>
2020-06-18 17:06:49 +0200

    Add routes to 2 pages Customers and Bills

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CustomersComponent } from './components/customers/customers.component';
+import { BillsComponent } from './components/bills/bills.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'customers',
+    component: CustomersComponent,
+  },
+  {
+    path: 'bills',
+    component: BillsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatButtonModule} from '@angular/material/button';
 
 import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
 import Amplify from 'aws-amplify';
 import awsmobile from '../aws-exports';
+import { CustomersComponent } from './components/customers/customers.component';
+import { BillsComponent } from './components/bills/bills.component';
 
 /* Configure Amplify resources */
 Amplify.configure(awsmobile);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomersComponent,
+    BillsComponent
   ],
   imports: [
     BrowserModule,
@@ -23,6 +28,7 @@ Amplify.configure(awsmobile);
     // Angular
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatButtonModule,
     // Amplify
     AmplifyUIAngularModule
   ],
