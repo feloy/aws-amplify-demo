676f2dc7ac3bbf46af052f88f3d403e1604d1764
Philippe Martin <contact@elol.fr>
2020-06-18 16:16:08 +0200

    Auth signin/signout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule} from '@angular/material/toolbar';
+
+import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
+import Amplify from 'aws-amplify';
+import awsmobile from '../aws-exports';
+
+/* Configure Amplify resources */
+Amplify.configure(awsmobile);
 
 @NgModule({
   declarations: [
@@ -12,7 +20,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    // Angular
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    // Amplify
+    AmplifyUIAngularModule
   ],
   providers: [],
   bootstrap: [AppComponent]
